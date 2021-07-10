30b9ad06fccc0c90cd8ab6421f7e16dcb4d10914
Kraujze <kraujze@inbox,ru>
2021-07-10 03:18:02 +0300

    fixed

--- src/app/gameform.java
+++ src/app/gameform.java
@@ -6,12 +6,12 @@ public class gameform extends JFrame {
     private JButton button1;
     private JButton button2;
     private JButton button3;
-    private JButton Button1;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
     private JButton button7;
-    private JButton Button;
     private JButton button8;
     private JButton button9;
-    private JButton button6;
     private JPanel panel1;
 
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
