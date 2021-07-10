b6ea03b578d73a3dcf86c71946b035149f588137
Kraujze <kraujze@inbox,ru>
2021-07-10 03:33:09 +0300

    commit

--- src/app/gameform.java
+++ src/app/gameform.java
@@ -1,6 +1,8 @@
 package app;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class gameform extends JFrame {
     private JButton button1;
@@ -23,6 +25,24 @@ public class gameform extends JFrame {
         setVisible(true);
         setSize(400,400);
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
+
+        button3.addActionListener(listener);
+        button6.addActionListener(listener);
+        button7.addActionListener(listener);
+        button1.addActionListener(listener);
+        button8.addActionListener(listener);
+        button2.addActionListener(listener);
+        button4.addActionListener(listener);
+        button9.addActionListener(listener);
+        button5.addActionListener(listener);
+
     }
 
 }
